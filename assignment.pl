1313eee1a38f28d82cd0681218684727940935b8
Chika <chkonyia@yahoo.com>
2017-12-09 15:12:18 +0000

    Add files via upload
    Assignment 4

--- assignment.pl
+++ assignment.pl
@@ -1,12 +1,31 @@
+%Question 1
 %Definitions for isElementInList(El, List)
-isElementInList([],[]).
-isElementInList(El,[El|[]]):- !.
+isElementInList([], []).
+isElementInList(El, [El | []]):- !.
 isElementInList(X, [El|List]):-
     X=El, !;
 isElementInList(X, List).
 
-%Definitions for reverseList(List, ReversedList)
 
-%insertElementIntoListEnd(El, List, NewList)
-
+%Question 2
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], List, List).
+mergeLists([EH | ET], List, [EH | Merged]):-
+mergeLists(ET, List, Merged).
+
+
+
+%Question 3
+%Definitions for reverseList(List, ReversedList)
+reverseList([], []).
+reverseList([EH | ET], List):-
+mergeLists(Reversed, [EH], List),
+reverseList(ET, Reversed).
+
+
+
+%Question 4  
+%insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList):-
+mergeLists(List, [El], NewList).
+
